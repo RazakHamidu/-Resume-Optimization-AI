4ee21dbf3f7918f5183e287f00cd1c11e18759e2
Razak Hamidu <razakgemer86@gmail.com>
2025-01-21 11:48:31 +0100

    Creare un componente di input di job descrizione

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import pdfplumber
 import streamlit as st
 
 
-
 def Uploader_Pdf_Exstrat_text(pdf_path):
     with pdfplumber.open(pdf_path) as pdf:  
         for page in pdf.pages:   
             return page.extract_text()
 
 uploaded_file = st.file_uploader("Carica un file PDF", type="pdf")
+
+job_description = st.text_area("Job_description")
